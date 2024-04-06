cc627413a2741e1cfc9dbee915d76ccb8839e955
Oleks <oleksb.dev@gmail.com>
2024-04-07 00:42:11 +0200

    elaborate telescope-ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,5 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
+ {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -22,5 +19,18 @@ return {
       require("telescope").load_extension("ui-select")
     end,
   },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+        require("telescope").setup ({
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown {}
+                }
+            }
+        })
+        require("telescope").load_extension("ui-select")
+    end
+  }
 }
 
