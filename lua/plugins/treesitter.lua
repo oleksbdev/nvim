7777d3a40f80d06058560e3ce017dc2aebca712e
Oleks <oleksb.dev@gmail.com>
2024-04-07 00:44:41 +0200

    add auto install option

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,6 +8,7 @@ return {
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+            -- auto_install = true,
             ensure_installed = {
                 "lua",
                 "vim",
