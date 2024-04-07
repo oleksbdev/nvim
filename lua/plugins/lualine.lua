d00c7c07dfcc8222c7195db6c0c174ab941a76f3
Oleks <oleksb.dev@gmail.com>
2024-04-08 01:26:06 +0200

    add wall clock time to the status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,49 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    require("lualine").setup({
+    require('lualine').setup {
       options = {
-        theme = "catppuccin-mocha",
-        sections = {
-          lualine_a = {
-            file = 1,
-          },
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+          -- "neo-tree",
         },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
       },
-    })
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        -- lualine_d = {{'datetime', style={'default'}}},
+        lualine_w = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_x = {'location'},
+        lualine_z = { {'datetime', style="%H:%M:%S" } }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_w = {'location'},
+        lualine_x = {},
+        lualine_y = {},
+        -- lualine_z = { {'datetime', style="%H:%M:%S" } }
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+  }
   end,
 }
